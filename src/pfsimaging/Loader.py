Update on stellar density calculation and fixed bugs
1. README.md: added explanation on how to calculate the systematic weights
2. calculate_imaging.py: updated function to calculate stellar density
3. get_gaia_bs.py: changed name
4. get_gaia_star.py: Added script to download gaia stars to calculate stellar density
5. train_nn.py: fixed bug
6. example_config.yaml: Added path to the Gaia stars directory
7. Loader.py: updated function to load the Gaia stars
8. imaging.py: updated function to calculate the stellar density
9. train.py, weights.py: fixed bugs

src/pfsimaging/Loader.py
@@ -4,6 +4,7 @@ from astropy.io import fits
 
 from pfstarget import cuts as Cuts
 import os
+from glob import glob
 
 import re
 
@@ -39,7 +40,7 @@ class Target(object):
     """
     def __init__(self, config):
         self.ra, self.dec, self.tract, self.patch, self.objectID = load_targets(config)
-        self.mask = np.zeros(len(self.ra))
+        self.mask = np.zeros(len(self.ra), dtype=bool)
         
     def load_bsmask(self, config):
         bsmask_file = os.path.join(config["bsmask"]["target_bsmask"] , "s23b_ssp_co_targets_bsmask.fits")
@@ -54,13 +55,22 @@ class Target(object):
             # bright star mask
             bsmask = halo | ghost | blooming
 
-            self.mask = match_bsmask(self.objectID, ID, bsmask)
+            mask = match_bsmask(self.objectID, ID, bsmask) #true if inside mask
+            self.mask |= mask
             
         else:
             print(f"does not have {bsmask_file}. Run GenerateStarMask.py")
             
     def load_bgmask(self, config):
-        print('bgmask not implemented yet')
+        bgmask_file = os.path.join(config["bgmask"]["target_bgmask"] , "s23b_ssp_co_targets_bgmask.fits")
+        if os.path.exists(bgmask_file):
+            with fits.open(bgmask_file) as hdu:
+                data = hdu[1].data
+            targetId = data['object_id']
+            mask = ~np.isin(self.objectID, targetId) #true if inside bgmask
+            self.mask |= mask
+        else:
+            print(f"does not have {bgmask_file}.")
         
         
     def get_tract(self, tract):
@@ -108,54 +118,43 @@ class Star(object):
         Load stellar objects and corresponding bright-star mask
         for the specified tract. config is a dictionary of path read from the config.yaml file in the config directory
     """
-    def __init__(self, tract, config):
-        self.tract = tract
-        self.ra, self.dec, self.patch, self.objectID = load_stars(tract, config)
-        if(self.ra is None):
-            self.mask = None
-        else:
-            self.mask = np.zeros_like(self.ra, dtype=bool)
-        
-    def load_bsmask(self, config):
-        maskfile = os.path.join(config["bsmask"]["star_bsmask"], f"{self.tract}.fits")
-        if os.path.exists(maskfile):
-            with fits.open(maskfile) as hdu:
-                if len(hdu)==2:
-                    data = hdu[1].data
-                    bsmaskID = data['id']
-                    bsmask = data['halo'] | data['ghost'] | data['blooming'] #bright star mask. true if 'inside' mask
-                    self.mask |= match_bsmask(self.objectID, bsmaskID, bsmask)
-                else:
-                    print(f'{maskfile} does not have objects')
-        else:
-            print(f'cannot open {maskfile}')
-            
-    def load_bgmask(self, config):
-        print('bgmask not implemented yet')
+    def __init__(self, field, config):
+        self.ra, self.dec, self.objectID = load_stars(field, config)
         
         
-def load_stars(tract, config):
-    filename = os.path.join(config["data"]["star_dir"], f"{tract}.fits")
-    if os.path.exists(filename):
-        with fits.open(filename) as hdu:
-            if len(hdu)==2:
+def load_stars(field, config):
+    directory = os.path.join(config["Gaia"]["star"], field)
+    star_files = glob(os.path.join(directory, "*.fits"))
+    
+    ra = []
+    dec = []
+    objectId = []
+    
+    if len(star_files)==0:
+        print("Gaia stars not downloaded yet. Run bin/get_gaia_stars.py")
+        return None, None, None
+    
+    else:
+        for file in star_files:
+            with fits.open(file) as hdu:
                 data = hdu[1].data
-                ID =data['object_id']
-                g = data['g_input_count']
-                r = data['r_input_count']
-                i = data['i_input_count']
-                z = data['z_input_count']
-                    
-                mask = (g >= 4) & (r >= 4) & (i >= 5) & (z >= 5)
-                    
-                ra = data['ra'][mask]%360
-                dec = data['dec'][mask]
-                patch = data['patch'][mask]
-                objectID = data['object_id'][mask]
-                    
-                return ra, dec, patch, objectID
-            else:
-                print(f"no objects in {filename}")                   
+            _ra = data['ra']
+            _dec = data['dec']
+            _objectId = data['source_id']
+            
+            ra.append(_ra)
+            dec.append(_dec)
+            objectId.append(_objectId)
+            
+        ra = np.concatenate(ra)
+        dec = np.concatenate(dec)
+        objectId = np.concatenate(objectId)
+
+        # get only unique sources
+        _, idx = np.unique(objectId, return_index=True)
+        dx = np.sort(idx)
+
+        return ra[idx], dec[idx], objectId[idx]
             
 class Random(object):
     """Container for HSC randoms　in a single tract.
@@ -204,7 +203,15 @@ class Random(object):
             print(f'cannot open {maskfile}')
             
     def load_bgmask(self, config):
-        print('bgmask not implemented yet')
+        bgmask_file = os.path.join(config["bgmask"]["random_bgmask"] , f"{self.tract}_bgmask.fits")
+        if os.path.exists(bgmask_file):
+            with fits.open(bgmask_file) as hdu:
+                data = hdu[1].data
+            targetId = data['object_id']
+            mask = np.isin(self.objectID, targetId) #true if inside bgmask
+            self.mask |= mask
+        else:
+            print(f"does not have {bgmask_file}.")
         
 def load_random(tract, config):
     filename = os.path.join(config["data"]["random_dir"], f"{tract}.fits")

src/pfsimaging/imaging.py
@@ -45,7 +45,7 @@ def get_property_all(tractpatch, dustmap, config):
     func = partial(get_property_tract, config = config)
     if len(tractlist)==0:
         print(f"No tracts in {tractpatch.field}")
-        return pd.DataFrame()
+        return None
     else:
         with Pool(processes=20) as pool:  # Adjust number of processes based on your CPU
             results = pool.map(func, tractlist)
@@ -61,7 +61,6 @@ def get_property_all(tractpatch, dustmap, config):
             properties = all_property.groupby('healpix').apply(lambda x: pd.Series(
                 {col: np.sum(x[col] * x['eff_area']) / np.sum(x['eff_area']) for col in all_columns} |  # Seeing, depth
                 {'area': np.sum(x['eff_area']) / np.sum(x['total']) * area}|
-                #{'star': np.log10(np.sum(x['star'])/(np.sum(x['eff_area'] / np.sum(x['total']) * area)))} |
                 {'total': np.sum(x['total'])})).reset_index()
             
             print(f'adding dust extinction for {dustmap}')
@@ -69,10 +68,13 @@ def get_property_all(tractpatch, dustmap, config):
             
             print('adding target density')
             properties = add_target_density(config, properties)
-            return property
+            
+            print('adding stellar density')
+            properties = add_stellar_density(config, tractpatch.field, properties)
+            return properties
         else:
             # return table if empty
-            return pd.DataFrame()
+            return None
     
     
 
@@ -131,9 +133,6 @@ def get_property_tract(tract,  config):
         return None
     properties = pd.merge(properties, patch_prop, on='healpix', how='left')
     
-    #############star file name
-    #print(f'adding stellar density on {tract}')
-    #properties = add_star_count(properties, tract,config)
     return properties
 
 def to_little_endian(arr):
@@ -261,7 +260,7 @@ def add_ext(properties, dust):
     return properties
     
 
-def add_star_count(properties, tract, config):
+def add_stellar_density(config, field, properties):
     """function to add stellar counts to pd properties
 
     Parameter
@@ -275,18 +274,15 @@ def add_star_count(properties, tract, config):
     with 'star' the total stellar count
     """
     
-    star = loader.Star(tract, config)
-    star.load_bsmask(config)
-    star.load_bgmask(config)
+    star = loader.Star(field, config)
     ra = star.ra
     dec = star.dec
-    mask = star.mask # true if "inside" bright star mask
         
     if ra is None:
-        print(f'No stars in tract {tract}')
+        print(f'No stars in {field} field')
         properties['star'] = np.zeros(len(properties['healpix']))
     else:
-        healpy = hp.ang2pix(nside=nside, theta=ra[~mask], phi=dec[~mask], lonlat=True)
+        healpy = hp.ang2pix(nside=nside, theta=ra, phi=dec, lonlat=True)
         
         _healpy, counts = np.unique(healpy, return_counts=True)
         data1 = {
@@ -295,7 +291,7 @@ def add_star_count(properties, tract, config):
         }
         table1 = pd.DataFrame(data1)
         properties = pd.merge(properties, table1, on='healpix', how='left')
-        properties['star'] = properties['star'].fillna(0)
+        properties['star'] = properties['star'].fillna(0)/area
     
     return properties
     
@@ -437,10 +433,15 @@ def clean_pixels(table, field):
     mask &= table['area'] > 0.0
     
     #remove healpix with nan
-    mask &= ~np.isnan(table.as_array().tolist()).any(axis=1)
+    mask = np.ones(len(table), dtype=bool)
+
+    for col in table.colnames:
+        if np.issubdtype(table[col].dtype, np.number):
+            mask &= ~np.isnan(table[col])
+
     
     #remove healpix with no star
-    mask &= mask['star'] > 0.0
+    mask &= table['star'] > 0.0
     
     return table[mask]
     

src/pfsimaging/train.py
@@ -1,16 +1,20 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
 
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 import torch.nn as nn
-import torch.nn.functional as F
+import copy
 import optuna
 
+import numpy as np
+
+import healpy as hp
+
 import os
+
+import json
 
 class CustomLoss(nn.Module):
     """
@@ -165,7 +169,7 @@ def make_objective(test_X, test_Y, test_Fpix, val_X, val_Y, val_Fpix, loader):
 
     return objective
 
-def run_optuna_nn(run_dir, property_df, keys, n_trials=200, top_k=5):
+def run_optuna_nn(run_dir, Property, keys, n_trials=200, top_k=5):
     """
     Function to train the neural network
     
@@ -191,7 +195,7 @@ def run_optuna_nn(run_dir, property_df, keys, n_trials=200, top_k=5):
     ------------------------------------------------------
     dictionary
     """
-    data = prepare_nn_data(property_df, keys)
+    data = prepare_nn_data(Property, keys)
     loader, val_data, test_data = make_tensors(data)
 
     val_X, val_Y, val_Fpix = val_data
@@ -215,7 +219,7 @@ def run_optuna_nn(run_dir, property_df, keys, n_trials=200, top_k=5):
 
     return study
 
-def prepare_nn_data(property_df, keys, nside=256, test_size=0.2):
+def prepare_nn_data(Property, keys, nside=256, test_size=0.2):
     """
     Function to prepare the train, validation and test datasets
     
@@ -242,16 +246,21 @@ def prepare_nn_data(property_df, keys, nside=256, test_size=0.2):
     4. input_dim: number of input imaging attributes
     5. scaler: scaler to normalize the imaging attributes
     """
-    properties = property_df[keys]
+    properties = Property[keys]
+    
+    if 'star' in keys:
+        properties['star'] = np.log10(properties['star'])
+    
+    df = properties.to_pandas()
 
     scaler = StandardScaler()
-    X_standardized = scaler.fit_transform(properties)
+    X_standardized = scaler.fit_transform(df)
 
-    mean = np.sum(df_cleaned["target"] * df_cleaned["area"]) / np.sum(df_cleaned["area"])
-    density = df_cleaned["target"] / mean
+    mean = np.sum(Property["target"] * Property["area"]) / np.sum(Property["area"])
+    density = Property["target"] / mean
 
     pix_area = hp.nside2pixarea(nside, degrees=True)
-    fpix = df_cleaned["area"] / pix_area
+    fpix = Property["area"] / pix_area
 
     X = X_standardized
 
@@ -272,6 +281,12 @@ def prepare_nn_data(property_df, keys, nside=256, test_size=0.2):
         "mean": mean,
     }
 
+def to_tensor_1d(x, dtype=torch.float):
+    return torch.from_numpy(np.asarray(x, dtype=float)).type(dtype).view(-1, 1)
+
+def to_tensor(x, dtype=torch.float):
+    return torch.from_numpy(np.asarray(x, dtype=float)).type(dtype)
+
 def make_tensors(data, batch_size=128):
     dtype = torch.float
 
@@ -279,17 +294,17 @@ def make_tensors(data, batch_size=128):
     val_X_np, val_Y_np, val_fpix_np = data["val"]
     test_X_np, test_Y_np, test_fpix_np = data["test"]
 
-    train_X = torch.from_numpy(train_X_np).type(dtype)
-    train_Y = torch.from_numpy(train_Y_np.values).type(dtype).view(-1, 1)
-    train_Fpix = torch.from_numpy(train_fpix_np.values).type(dtype)
+    train_X = to_tensor(train_X_np, dtype)
+    train_Y = to_tensor_1d(train_Y_np, dtype)
+    train_Fpix = to_tensor(train_fpix_np, dtype)
 
-    val_X = torch.from_numpy(val_X_np).type(dtype)
-    val_Y = torch.from_numpy(val_Y_np.values).type(dtype).view(-1, 1)
-    val_Fpix = torch.from_numpy(val_fpix_np.values).type(dtype)
+    val_X = to_tensor(val_X_np, dtype)
+    val_Y = to_tensor_1d(val_Y_np, dtype)
+    val_Fpix = to_tensor(val_fpix_np, dtype)
 
-    test_X = torch.from_numpy(test_X_np).type(dtype)
-    test_Y = torch.from_numpy(test_Y_np.values).type(dtype).view(-1, 1)
-    test_Fpix = torch.from_numpy(test_fpix_np.values).type(dtype)
+    test_X = to_tensor(test_X_np, dtype)
+    test_Y = to_tensor_1d(test_Y_np, dtype)
+    test_Fpix = to_tensor(test_fpix_np, dtype)
 
     loader = DataLoader(
         TensorDataset(train_X, train_Y, train_Fpix),

src/pfsimaging/weights.py
@@ -3,10 +3,19 @@ import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import mean_squared_error
 
 import torch
+
+import pandas as pd
+
+from astropy.table import Table
+
+import os
+
+from pfsimaging import train as train
 
 #Calculate systematic weights using
 #1. Linear Regression
@@ -16,6 +25,9 @@ import torch
 def prepare_data(df, keys):
     df_cleaned = df.dropna()
     properties = df_cleaned[keys]
+    if 'star' in keys:
+        properties = properties.copy()
+        properties.loc[:, 'star'] = np.log10(properties['star'])
     
     scaler = StandardScaler()
     df_standardized = pd.DataFrame(scaler.fit_transform(properties), columns= properties.columns)
@@ -26,7 +38,7 @@ def prepare_data(df, keys):
     X = np.concatenate([np.array(df_standardized[key]).reshape(-1, 1) for key in keys], axis=1)
 
     train_X, test_X, train_Y, test_Y = train_test_split(X, density)
-    return train_X, test_X, train_Y, test_Y
+    return train_X, test_X, train_Y, test_Y, X
     
 
 def linear_weights(Property, keys):
@@ -47,7 +59,7 @@ def linear_weights(Property, keys):
     """
     df = Property.to_pandas()
     df_cleaned = df.dropna()
-    train_X, test_X, train_Y, test_Y = prepare_data(df, keys)
+    train_X, test_X, train_Y, test_Y, X = prepare_data(df, keys)
 
     #learn using linear regression
     regr = LinearRegression()
@@ -91,7 +103,7 @@ def quadratic_weights(Property, keys):
     """
     df = Property.to_pandas()
     df_cleaned = df.dropna()
-    train_X, test_X, train_Y, test_Y = prepare_data(df, keys)
+    train_X, test_X, train_Y, test_Y, X = prepare_data(df, keys)
     
     # Transform features to polynomial (degree=2)
     poly = PolynomialFeatures(degree=2)
@@ -148,13 +160,16 @@ def nn_weights(Property, keys, best_model):
     else:
         print(f"{filename} does not exist")
         return None
-    model = build_model(**checkpoint["config"])
+    model = train.build_model(**checkpoint["config"])
     model.load_state_dict(checkpoint["state_dict"])
     model.eval()
     
     df = Property.to_pandas()
     df_cleaned = df.dropna(subset=['target'])
     properties = df_cleaned[keys]
+    if "star" in properties:
+        properties = properties.copy()
+        properties.loc[:, 'star'] = np.log10(properties['star'])
     scaler = StandardScaler()
     df_standardized = pd.DataFrame(scaler.fit_transform(properties), columns= properties.columns)
 