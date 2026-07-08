3b199837664337018283b88407cfe42cc739f6c1
changhoonhahn <changhoon.hahn@utexas.edu>
2026-07-08 19:00:41 +0900

    cleaned up script a bit; out_dir is taken directly as input, not from config to avoid overwriting issues

--- bin/calculate_imaging.py
+++ bin/calculate_imaging.py
@@ -1,26 +1,30 @@
+import os
+import yaml
+import argparse
+from astropy.table import Table, vstack
+
 from pfsimaging import imaging as Im
 from pfsimaging import Loader as loader
 
-from astropy.table import Table, vstack
-
-import argparse
-
-import os
-
-import yaml
 
 def parse_args():
     ap = argparse.ArgumentParser(description=__doc__)
     ap.add_argument('--config', required=True,
                     help='path to the config file which specify necessary path')
+    ap.add_argument('--out-dir', required=True, 
+            help='output path')
     ap.add_argument('--dustmap', default='csfd_desi',
                     choices=['csfd_desi', 'sfd'],
                     help='dust map for dust attenuation (default: csfd_desi)')
     ap.add_argument('--fields', nargs='+', default=['spring', 'autumn'],
                     choices=['spring', 'autumn', 'hectomap'],
                     help='which fields to process (default: spring autumn)')
+    ap.add_argument('--nprocess', type=int, default=1,
+                    help='number of processes for multiprocessing')
     ap.add_argument('--clean', '-c', action='store_true',
                         help='remove problematic pixels')
+    ap.add_argument('--verbose', action='store_true', 
+            help='enable print statements') 
     return ap.parse_args()
 
 
@@ -32,42 +36,42 @@ def main():
     with open(path, "r") as f:
         config = yaml.safe_load(f)
     
-    out_dir = config['out_dir']['imaging']
-    os.makedirs(out_dir, exist_ok=True)
-    
     dustmap = args.dustmap
     fields = args.fields
     
     tables = []
     cleaned_tables = []
     for field in fields:
+        if args.verbose: print(f'processing {field}') 
         TractPatch = loader.TractPatch(field)
         
-        file = os.path.join(out_dir,f'{field}_property.fits')
-        im_property = Im.get_property_all(TractPatch, dustmap, config)
+        im_property = Im.get_property_all(TractPatch, dustmap, config, 
+                nprocess=args.nprocess, 
+                verbose=args.verbose)
         
         if im_property is None:
             print(f"no tract in {field} field")
         else:
             table = Table.from_pandas(im_property)
-            table.write(file, format='fits', overwrite=True)
+            table.write(os.path.join(args.out_dir, f'{field}_property.fits'), 
+                    format='fits', overwrite=True)
             
             tables.append(table)
             
             if (args.clean):
-                cleaned_file = os.path.join(out_dir,f'{field}_property_cleaned.fits')
                 cleaned_table = Im.clean_pixels(table, field)
-                cleaned_table.write(cleaned_file, format='fits', overwrite=True)
+                cleaned_table.write(os.path.join(args.out_dir,f'{field}_property_cleaned.fits'), 
+                        format='fits', overwrite=True)
                 cleaned_tables.append(cleaned_table)
      
     all_table = vstack(tables)
-    file = os.path.join(out_dir,'all_property.fits')
-    all_table.write(file, format='fits', overwrite=True)
+    all_table.write(os.path.join(args.out_dir, 'all_property.fits'), 
+            format='fits', overwrite=True)
     
     if (args.clean):
         all_cleaned_table = vstack(cleaned_tables)
-        cleaned_file = os.path.join(out_dir,f'all_property_cleaned.fits')
-        all_cleaned_table.write(cleaned_file, format='fits', overwrite=True)
+        all_cleaned_table.write(os.path.join(args.out_dir, 'all_property_cleaned.fits'), 
+                format='fits', overwrite=True)
         
         
     
