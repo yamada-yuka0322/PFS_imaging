Fixed bugs on NN weight calculation
1. train.py: fixed f_pix data shape shape(1) to shape(N, 1)
2. weights.py: log scaled E(B-V9 and target density
3. statistic.py: added density as a function of imaging attributes with poisson error bars
4. imaging.py: included randoms in tracts outside the observed region, but are overlapping with healpixels with non-zero effective observing area
5. calculate_imaging.py, calculate_weights.py: added arguments so that the user can directly specify the directory path of the input imaging properties and the output files.

src/pfsimaging/imaging.py
@@ -8,7 +8,6 @@ from astropy.table import Table, vstack, join
 from multiprocessing import Pool
 
 from functools import partial
-import pandas as pd
 
 from pfsimaging import Loader as loader
 
@@ -59,16 +58,37 @@ def get_property_all(tractpatch, dustmap, config, nprocess=1, verbose=False):
         valid_results = [res for res in results if res is not None]
 
         if valid_results:
-            all_property = pd.concat(valid_results, ignore_index=True)
+            all_property = vstack(valid_results)
 
             # When healpixels are overlapping between different tracts
             # take the area weighted average between the effective overlapping 
             # area for all of the tracts 
-            all_columns = property_name
-            properties = all_property.groupby('healpix').apply(lambda x: pd.Series(
-                {col: np.sum(x[col] * x['eff_area']) / np.sum(x['eff_area']) for col in all_columns} |  # Seeing, depth
-                {'area': np.sum(x['eff_area']) / np.sum(x['total']) * area}|
-                {'total': np.sum(x['total'])})).reset_index()
+            
+            groups = all_property.group_by('healpix').groups
+
+            rows = []
+
+            for g in groups:
+                row = {}
+                row['healpix'] = g['healpix'][0]
+
+                eff_area = np.asarray(g['eff_area']) #normalized effective area
+                t_eff_area = np.sum(eff_area)
+
+                # effective area weighted mean of imaging attributes
+                for col in property_name:
+                    x = np.asarray(g[col]) #imaging attribute
+                    row[col] = np.sum(x * eff_area) / t_eff_area
+
+                # area
+                row['area'] = t_eff_area / np.sum(g['total']) * area #effective area
+
+                # total
+                row['total'] = np.sum(g['total']) #total number of randoms
+
+                rows.append(row)
+
+            properties = Table(rows)
             
             if verbose: print(f'adding dust extinction for {dustmap}')
             properties = add_ext(properties, dustmap)
@@ -110,35 +130,46 @@ def get_property_tract(tract, config=None, verbose=False):
     all_healpix = to_little_endian(all_healpix)
     all_patch = to_little_endian(all_patch)
     
+    if (len(all_ra)==0):
+        print(f"tract {tract} is outside observed footprint")
+        return None
+    
     randoms = loader.Random(tract, config)
     randoms.load_bsmask(config)
     randoms.load_bgmask(config)
-    
     random_mask = randoms.mask # true if "inside" masked region
-    if(random_mask is None) or (np.sum(random_mask) == len(random_mask)):
-        print(f"No effective observation area in tract {tract}")
-        return None
-    random_ID = randoms.objectID[~random_mask] #objectID of the randoms that were outside masks
     
-    df = pd.DataFrame({
+    if(randoms.ra is None) or (np.sum(random_mask) == len(random_mask)):
+        #Has randoms but all of them were inside masks or outside footprint
+        print(f"No effective observation area in tract {tract}")
+        mask = np.zeros(len(all_ID), dtype=bool)
+    else:
+        random_ID = randoms.objectID[~random_mask] #objectID of the randoms that were outside masks
+        mask = np.isin(all_ID, random_ID)
+                       
+    table = Table({
         'healpix': all_healpix,
-        'mask': np.isin(all_ID, random_ID), #true if 'outside' masks
+        'mask': mask, #true if 'outside' masks
         'patch': all_patch,
     })
+    
+    #group all the randoms into healpixels
+    grouped = table.group_by("healpix").groups
 
-    properties = (
-        df.groupby('healpix')
-        .agg(
-            total=('mask', 'size'),   # total random count before masking
-            eff_area=('mask', 'sum')    # random count after masking
-        )
-        .reset_index()
-    )
+    rows = []
+    for g in grouped:
+        rows.append({
+            "healpix": g["healpix"][0],
+            "total": len(g),               # mask の size
+            "eff_area": np.sum(g["mask"])  # mask の sum
+        })
 
-    patch_prop = band_property(tract, config, df)
+    properties = Table(rows)
+
+    patch_prop = band_property(tract, config, table)
     if patch_prop is None:
         return None
-    properties = pd.merge(properties, patch_prop, on='healpix', how='left')
+    properties = join(properties, patch_prop, keys='healpix', join_type='left')
     
     return properties
 
@@ -150,7 +181,7 @@ def to_little_endian(arr):
     return arr
 
 
-def band_property(tract, config, df):
+def band_property(tract, config, table):
     """
     function to add the g,r,i,z,y-depth and seeing of each healpixels
     
@@ -159,8 +190,8 @@ def band_property(tract, config, df):
     tract: int
     ID of tract considered
     
-    df: PdDataframe
-    dataframe including the randoms in the concidered tract
+    table: astropy table
+    Table including the randoms in the concidered tract
     column: 'healpix', 'mask', 'patch'
     1. healpix: which healpix the random is included in
     2. mask: true if outside mask
@@ -168,8 +199,8 @@ def band_property(tract, config, df):
 
     output
     ------------------------------------------
-    t:PdDataframe
-    dataframe  of imaging properties for each randoms points
+    t:astropy table
+    Table  of imaging properties for each randoms points
     defined by cross matching patch ID
     """
     patches = loader.Patches()
@@ -180,16 +211,41 @@ def band_property(tract, config, df):
         print(f"patch column missing in tract {tract} property")
         return None
 
-    df_valid = df[df['mask']] #Get randoms outside the masked area
+    table_valid = table[table['mask']] #Get randoms outside the masked area
+    
+    if(np.sum(table['mask']) == 0):#If there are no randoms outside mask (effective area = 0)
+        # set all the imaging attributes as zero
+        # This will not matter because the effective area is 0
+        u, indices = np.unique(table['healpix'], return_index=True)
+        patch_prop = Table({
+            'healpix':u,
+            'patch':table['patch'][indices]
+        })
+        for col in property_name:
+            patch_prop[col] = np.zeros(len(u))
+        return patch_prop
 
-    patch_prop = pd.merge(
-        df_valid[['healpix','patch']],
+    patch_prop = join(
+        table_valid[['healpix','patch']],
         Property,
-        on='patch',
-        how='left'
+        keys='patch',
+        join_type='left'
     )
 
-    patch_prop = patch_prop.groupby('healpix')[property_name].mean().reset_index()
+    #take the average per heal pixel
+    groups = patch_prop.group_by('healpix').groups
+
+    rows = []
+
+    for g in groups:
+        row = {'healpix': g['healpix'][0]}
+
+        for col in property_name:
+            row[col] = np.mean(g[col])
+
+        rows.append(row)
+
+    patch_prop = Table(rows)
     return patch_prop
    
 
@@ -285,9 +341,11 @@ def add_stellar_density(config, field, properties):
             'healpix':_healpy,
             'star' : counts
         }
-        table1 = pd.DataFrame(data1)
-        properties = pd.merge(properties, table1, on='healpix', how='left')
-        properties['star'] = properties['star'].fillna(0)/area
+        table1 = Table(data1)
+        properties = join(properties, table1, keys='healpix', join_type='left')
+
+        properties['star'] = np.ma.filled(properties['star'], 0).astype(float)
+        properties['star'] /= area
     
     return properties
     
@@ -382,11 +440,12 @@ def add_target_density(config, properties):
         
         # count the number of galaxies in each healpix
         _healpy, counts = np.unique(healpix, return_counts=True)
-        data1 = pd.DataFrame({'healpix':_healpy, 'target' : counts})
+        table1 = Table({'healpix':_healpy, 'target' : counts})
         
-        merged = pd.merge(properties, data1, on='healpix', how='left')
+        merged = join(properties, table1, keys='healpix', join_type='left')
         
-        merged = merged.fillna({'target': 0})
+        merged['target'] = merged['target'].filled(0)
+        merged['target'] = merged['target'].astype(float)
         merged['target'] /= merged['area']
         return merged
     else:
@@ -399,10 +458,9 @@ def anomaly():
     dec_deg = 0.2
     radius_deg = 0.85
 
-    # 中心方向ベクトル
     vec = hp.ang2vec(ra_deg, dec_deg, lonlat=True)
 
-    # 円内の healpix index
+    # healpix index within radius_deg
     pix = hp.query_disc(
         nside,
         vec,
@@ -413,29 +471,58 @@ def anomaly():
 
     return pix
 
-def clean_pixels(table, field):
+def clean_pixels(table, field, verbose=False):
+    if verbose:
+        total = len(table['healpix'])
+        print(f"Original pixel count is {total} ")
+    
     #remove anomaly
     if(field == 'spring'):
         anomaly_pix = anomaly()
         mask = ~np.isin(table['healpix'], anomaly_pix)
-    else:
-        mask = np.ones_like(table['healpix'], dtype=bool) 
+        table = table[mask]
+        
+        if verbose:
+            masked = total - np.sum(mask)
+            print(f"Removed {masked} pixels in anomalous region in {field} field")
+            
+    #remove healpix with small effective area
+    mask = table['area'] > 0.0
+    
+    if verbose:
+        total = len(table['healpix'])
+        masked = total - np.sum(mask)
+        print(f"Removed {masked} pixels with 0 effective area in {field} field")
+    table = table[mask]
         
     #remove healpix with small random count
     mean_count = area * 3600.0 * 100.0
-    mask &= table['total'] > mean_count / 2.0
-    
-    #remove healpix with small effective area
-    mask &= table['area'] > 0.0
+    mask = table['total'] > mean_count / 2.0
+    if verbose:
+        total = len(table['healpix'])
+        masked = total - np.sum(mask)
+        print(f"Removed {masked} pixels with small random count in {field} field")
+    table = table[mask]
 
     #remove healpix with nan
+    mask = np.ones(len(table['healpix']), dtype=bool)
     for col in table.colnames:
         if np.issubdtype(table[col].dtype, np.number):
             mask &= ~np.isnan(table[col])
 
+    if verbose:
+        total = len(table['healpix'])
+        masked = total - np.sum(mask)
+        print(f"Removed {masked} pixels with nans in {field} field")
+    table = table[mask]
     
     #remove healpix with no star
-    mask &= table['star'] > 0.0
+    mask = table['star'] > 0.0
+    if verbose:
+        total = len(table['healpix'])
+        masked = total - np.sum(mask)
+        print(f"Removed {masked} pixels with 0 stars in {field} field")
+    table = table[mask]
     
-    return table[mask]
+    return table
     

src/pfsimaging/train.py
@@ -33,14 +33,14 @@ class CustomLoss(nn.Module):
         3. fpix: completeness of each pixel
     
     """
-    def __init__(self, model):
+    def __init__(self):
         super().__init__()
-        self.model = model
 
     def forward(self, output, target, fpix):
-        weighted_mse = torch.mean(fpix * (output - target) ** 2)
-
-        return weighted_mse
+        
+        squared_error = (output - target) ** 2
+        
+        return torch.sum(fpix * squared_error) / torch.sum(fpix)
     
 
 def build_model(input_dim, width, depth):
@@ -115,12 +115,12 @@ def make_objective(test_X, test_Y, test_Fpix, val_X, val_Y, val_Fpix, loader):
             Minimum validation loss achieved during training.
         """
         # suggest hiperparameters
-        lr = trial.suggest_float("lr", 1e-6, 1e-3, log=True)
-        depth = trial.suggest_int("depth", 2, 6)
-        width = trial.suggest_categorical("width", [64, 128, 256, 512, 1024])
+        lr = trial.suggest_float("lr", 1e-5, 1e-3, log=True)
+        depth = trial.suggest_int("depth", 2, 4)
+        width = trial.suggest_categorical("width", [16, 32, 64, 128])
 
         model = build_model(12, width, depth)
-        loss_fn = CustomLoss(model)
+        loss_fn = CustomLoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
         best_val_loss = float("inf")
@@ -246,31 +246,65 @@ def prepare_nn_data(Property, keys, nside=256, test_size=0.2):
     4. input_dim: number of input imaging attributes
     5. scaler: scaler to normalize the imaging attributes
     """
-    properties = Property[keys]
+    cleaned = Property[Property['target']>0.0]
+
+    properties = cleaned[keys]
     
     if 'star' in keys:
         properties['star'] = np.log10(properties['star'])
+        
+    if 'csfd_desi_extinction' in keys:
+        properties['csfd_desi_extinction'] = np.log10(properties['csfd_desi_extinction'])
     
     df = properties.to_pandas()
 
-    scaler = StandardScaler()
-    X_standardized = scaler.fit_transform(df)
+    #scaler = StandardScaler()
+    #X_standardized = scaler.fit_transform(df)
 
-    mean = np.sum(Property["target"] * Property["area"]) / np.sum(Property["area"])
-    density = Property["target"] / mean
+    mean = np.sum(cleaned["target"] * cleaned["area"]) / np.sum(cleaned["area"])
+    density = np.log10(cleaned["target"] / mean)
 
     pix_area = hp.nside2pixarea(nside, degrees=True)
-    fpix = Property["area"] / pix_area
+    fpix = cleaned["area"] / pix_area
 
-    X = X_standardized
+    X = df.to_numpy(dtype=np.float64)
+    density = np.asarray(density, dtype=np.float64)
+    fpix = np.asarray(fpix, dtype=np.float64)
 
-    train_X_np, temp_X_np, train_Y_np, temp_Y_np, train_fpix_np, temp_fpix_np = train_test_split(
-        X, density, fpix, test_size=test_size
+    (
+        train_X_np,
+        temp_X_np,
+        train_Y_np,
+        temp_Y_np,
+        train_fpix_np,
+        temp_fpix_np,
+    ) = train_test_split(
+        X,
+        density,
+        fpix,
+        test_size=test_size,
+        random_state=42,
     )
 
-    val_X_np, test_X_np, val_Y_np, test_Y_np, val_fpix_np, test_fpix_np = train_test_split(
-        temp_X_np, temp_Y_np, temp_fpix_np, test_size=0.5
+    (
+        val_X_np,
+        test_X_np,
+        val_Y_np,
+        test_Y_np,
+        val_fpix_np,
+        test_fpix_np,
+    ) = train_test_split(
+        temp_X_np,
+        temp_Y_np,
+        temp_fpix_np,
+        test_size=0.5,
+        random_state=42,
     )
+
+    scaler = StandardScaler()
+    train_X_np = scaler.fit_transform(train_X_np)
+    val_X_np = scaler.transform(val_X_np)
+    test_X_np = scaler.transform(test_X_np)
 
     return {
         "train": (train_X_np, train_Y_np, train_fpix_np),
@@ -296,15 +330,15 @@ def make_tensors(data, batch_size=128):
 
     train_X = to_tensor(train_X_np, dtype)
     train_Y = to_tensor_1d(train_Y_np, dtype)
-    train_Fpix = to_tensor(train_fpix_np, dtype)
+    train_Fpix = to_tensor_1d(train_fpix_np, dtype)
 
     val_X = to_tensor(val_X_np, dtype)
     val_Y = to_tensor_1d(val_Y_np, dtype)
-    val_Fpix = to_tensor(val_fpix_np, dtype)
+    val_Fpix = to_tensor_1d(val_fpix_np, dtype)
 
     test_X = to_tensor(test_X_np, dtype)
     test_Y = to_tensor_1d(test_Y_np, dtype)
-    test_Fpix = to_tensor(test_fpix_np, dtype)
+    test_Fpix = to_tensor_1d(test_fpix_np, dtype)
 
     loader = DataLoader(
         TensorDataset(train_X, train_Y, train_Fpix),

src/pfsimaging/weights.py
@@ -23,22 +23,26 @@ from pfsimaging import train as train
 #3. Neural Network (from optuna best model)
 
 def prepare_data(df, keys):
-    df_cleaned = df.dropna()
+    df_cleaned = df[df['target']>0.0]
     properties = df_cleaned[keys]
     if 'star' in keys:
         properties = properties.copy()
         properties.loc[:, 'star'] = np.log10(properties['star'])
+        
+    if 'csfd_desi_extinction' in keys:
+        properties = properties.copy()
+        properties.loc[:, 'csfd_desi_extinction'] = np.log10(properties['csfd_desi_extinction'])
     
     scaler = StandardScaler()
     df_standardized = pd.DataFrame(scaler.fit_transform(properties), columns= properties.columns)
     
     mean = np.sum(df_cleaned["target"]*df_cleaned["area"])/np.sum(df_cleaned["area"])
-    density = df_cleaned["target"]/mean
+    density = np.log10(df_cleaned["target"]/mean)
 
     X = np.concatenate([np.array(df_standardized[key]).reshape(-1, 1) for key in keys], axis=1)
 
     train_X, test_X, train_Y, test_Y = train_test_split(X, density)
-    return train_X, test_X, train_Y, test_Y, X
+    return train_X, test_X, train_Y, test_Y, X, df_cleaned.index
     
 
 def linear_weights(Property, keys):
@@ -59,7 +63,7 @@ def linear_weights(Property, keys):
     """
     df = Property.to_pandas()
     df_cleaned = df.dropna()
-    train_X, test_X, train_Y, test_Y, X = prepare_data(df, keys)
+    train_X, test_X, train_Y, test_Y, X, idx = prepare_data(df, keys)
 
     #learn using linear regression
     regr = LinearRegression()
@@ -77,12 +81,15 @@ def linear_weights(Property, keys):
     # MSE for test data
     mse_test = mean_squared_error(test_Y, predicted_density)
     print("Test MSE linear:", mse_test)
+    
+    log_density = regr.predict(X)
+    _weight = 1/10**log_density
 
-    _density = regr.predict(X)
-    _weight = 1/_density
+    df_out = df.copy()
+    df_out['lin_weight'] = 0.0
+    df_out.loc[idx, 'lin_weight'] = _weight
 
-    df_cleaned['lin_weight'] = _weight
-    table = Table.from_pandas(df_cleaned)
+    table = Table.from_pandas(df_out)
     return table
 
 def quadratic_weights(Property, keys):
@@ -103,7 +110,7 @@ def quadratic_weights(Property, keys):
     """
     df = Property.to_pandas()
     df_cleaned = df.dropna()
-    train_X, test_X, train_Y, test_Y, X = prepare_data(df, keys)
+    train_X, test_X, train_Y, test_Y, X, idx = prepare_data(df, keys)
     
     # Transform features to polynomial (degree=2)
     poly = PolynomialFeatures(degree=2)
@@ -128,12 +135,16 @@ def quadratic_weights(Property, keys):
     print("Training MSE quadratic:", mse_train)
     print("Test MSE quadratic:", mse_test)
 
-    _density = regr.predict(X_poly)
-    _weight = 1/_density
-    
-    df_cleaned['quad_weight'] = _weight
-    table = Table.from_pandas(df_cleaned)
+    log_density = regr.predict(X_poly)
+    _weight = 1/10**log_density
+
+    df_out = df.copy()
+    df_out['quad_weight'] = 0.0
+    df_out.loc[idx, 'quad_weight'] = _weight
+
+    table = Table.from_pandas(df_out)
     return table
+
 
 
 def nn_weights(Property, keys, best_model):
@@ -167,9 +178,14 @@ def nn_weights(Property, keys, best_model):
     df = Property.to_pandas()
     df_cleaned = df.dropna(subset=['target'])
     properties = df_cleaned[keys]
+    
     if "star" in properties:
         properties = properties.copy()
         properties.loc[:, 'star'] = np.log10(properties['star'])
+    if 'csfd_desi_extinction' in properties:
+        properties = properties.copy()
+        properties.loc[:, 'csfd_desi_extinction'] = np.log10(properties['csfd_desi_extinction'])
+        
     scaler = StandardScaler()
     df_standardized = pd.DataFrame(scaler.fit_transform(properties), columns= properties.columns)
 
@@ -180,8 +196,8 @@ def nn_weights(Property, keys, best_model):
         pred = model(X_tensor).squeeze().detach().cpu().numpy()
 
     #density = np.clip(pred, 1e-6, None)
-    density = pred
-    _weight = np.where(density <= 0, np.nan, 1.0 / density)
+    log_density = pred
+    _weight = 10**(-log_density)
     
     df_cleaned['nn_weight'] = _weight
     table = Table.from_pandas(df_cleaned)